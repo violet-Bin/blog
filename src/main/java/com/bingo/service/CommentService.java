348ae04341257a368d7c835604d1964f04782767
bingo <18845649698@163.com>
2019-12-08 22:14:42 +0800

    modified

---
+++ src/main/java/com/bingo/service/CommentService.java
@@ -0,0 +1,15 @@
+package com.bingo.service;
+
+import com.bingo.po.Comment;
+
+import java.util.List;
+
+/**
+ * Created by limi on 2017/10/22.
+ */
+public interface CommentService {
+
+    List<Comment> listCommentByBlogId(Long blogId);
+
+    Comment saveComment(Comment comment);
+}
---
+++ src/main/java/com/bingo/service/UserService.java
@@ -0,0 +1,11 @@
+package com.bingo.service;
+
+import com.bingo.po.User;
+
+/**
+ * Created by limi on 2017/10/15.
+ */
+public interface UserService {
+
+    User checkUser(String username, String password);
+}
